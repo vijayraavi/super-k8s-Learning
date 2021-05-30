880c408f42d5ed4ed40bf2979811920dc4cbcf7a
Vijay Raavi <vijay.raavi@pennywisesolutions.com>
2021-05-30 13:15:11 +0530

    Bug fix.

--- kubeadm/Setup-Kubeadm-AzureVM/kubeadm-deploy-vm.sh
+++ kubeadm/Setup-Kubeadm-AzureVM/kubeadm-deploy-vm.sh
@@ -4,7 +4,7 @@ location="centralindia"
 deploymentName="kubeadm-vm-deploy"
 
 # download template
-wget https://raw.githubusercontent.com/github.com/vijayraavi/super-k8s-Learning/main/kubeadm/Setup-Kubeadm-AzureVM/kubeadm-cluster-deploy.json -O template.json
+wget https://raw.githubusercontent.com/vijayraavi/super-k8s-Learning/main/kubeadm/Setup-Kubeadm-AzureVM/kubeadm-cluster-deploy.json -O template.json
 
 # create resource group
 az group create -n $resourceGroupName -l $location
